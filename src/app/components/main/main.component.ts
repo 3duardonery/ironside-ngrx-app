861f3cc99da4710489030844be13ddc00b27d9f1
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-04-18 08:28:50 -0300

    add logic to save some name

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
 export class MainComponent implements OnInit {
+  listForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+  });
+
+  names: string[] = [];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  submit(): void {
+    console.log(this.listForm.value);
+  }
 }
