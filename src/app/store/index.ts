81e9cf2ee51a15d65d3331a79424acec742ca127
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-04-20 07:40:00 -0300

    add metareducer function

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -16,4 +16,13 @@ export const reducers: ActionReducerMap<State> = {
   [fromGuestReducer.guestsFeatureKey]: fromGuestReducer.reducer,
 };
 
-export const metaReducers: MetaReducer<State>[] = isDevMode() ? [] : [];
+export const metaReducers: MetaReducer<State>[] = isDevMode() ? [debug] : [];
+
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function (state, action) {
+    console.log('state', state);
+    console.log('action', action);
+
+    return reducer(state, action);
+  };
+}
